a56cf00163968f88e5f967d451a4f5847e759618
estefanomisme <3401@holbertonschool.com>
2021-09-07 07:35:07 +0000

    python + C cycle linked lists

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * check_cycle - check if a linked list has a cycle or not
+ * @list: a linked list
+ *
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *ptr = list;
+	while (ptr != NULL)
+	{
+		if (ptr->next == list)
+			return (1);
+		ptr = ptr->next;
+	}
+	return (0);
+}
