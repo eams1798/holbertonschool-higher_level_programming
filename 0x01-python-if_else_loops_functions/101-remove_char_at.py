05803b20509acb8eff30adb0bc6e303f9998dfae
estefanomisme <3401@holbertonschool.com>
2021-09-08 17:25:31 +0000

    pep8 and requiwqrement errors

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -3,5 +3,5 @@ def remove_char_at(string, n):
     if n >= 0:
         c = string[0:n] + string[n+1:]
     else:
-        c= string
+        c = string
     return c
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(10):
-        if (i * 10 + j) != 99:
-            print("{:d}{:d}".format(i, j), end=(", "))
+for i in range(100):
+        if i != 99:
+            print(("0" if i < 10 else "") + "{:d}".format(i), end=(", "))
         else:
-            print("{:d}{:d}".format(i, j))
+            print("{:d}".format(i))
