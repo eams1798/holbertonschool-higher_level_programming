113dffb8ef0807e8c0f6d75dc9d21960174e1ed2
estefanomisme <3401@holbertonschool.com>
2021-09-10 06:30:58 +0000

    2-args.py pep8

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import sys
     if (len(sys.argv) - 1 < 1):
-        print("0 arguments.");
+        print("0 arguments.")
     elif (len(sys.argv) - 1 > 1):
         print("{:d} arguments:".format(len(sys.argv) - 1))
     else:
