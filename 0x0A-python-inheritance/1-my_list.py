ac017c7284e7ed99d9e8c6dd6b8f57d4d07d3465
estefanomisme <3401@holbertonschool.com>
2021-10-12 10:11:09 +0000

    python inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -2,9 +2,16 @@
 """1. My list"""
 
 
-def print_sorted(self):
-    """prints a sorted list"""
-    lst = self.copy()
-    lst.sort()
-    print(lst)
-    lst.clear()
+class MyList(list):
+    """a list that inherits from a list"""
+
+    def __init__(self):
+        """initializer for MyList"""
+        pass
+
+    def print_sorted(self):
+        """prints a sorted list"""
+        lst = self.copy()
+        lst.sort()
+        print(lst)
+        lst.clear()
