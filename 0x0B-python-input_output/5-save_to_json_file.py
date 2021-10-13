13d2f6bd5233257159597ab6e6a8dab67eead3ba
estefanomisme <3401@holbertonschool.com>
2021-10-13 07:19:54 +0000

    adding documentation

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+"""5. Save Object to a file"""
+
+
 import json
 
+
 def save_to_json_file(my_obj, filename):
+    """Writes an Object to a text file, using a JSON representation
+
+    Args:
+        my_obj: a object to save in the file
+        filename: the name of a file
+    """
     with open(filename, 'w') as f:
         f.write(json.dumps(my_obj))
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-import sys, os.path
+"""7. A script that adds all arguments to a Python list, and then save
+them to a file"""
+
+
+import sys
+import os.path
 
 save_to = __import__('5-save_to_json_file').save_to_json_file
 load_from = __import__('6-load_from_json_file').load_from_json_file
