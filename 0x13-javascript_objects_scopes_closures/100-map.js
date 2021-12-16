761d14e79e94786ff1e97fae8e8d302fa6de4350
estefanomisme <3401@holbertonschool.com>
2021-12-16 07:41:52 +0000

    file not found?

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,7 +2,7 @@
 /*
  * imports an array and computes a new array
  */
-const list = require('./100-data').list;
+const list = require('./100-data.js').list;
 console.log(list);
 const list2 = list.map(x => x * list.indexOf(x));
 console.log(list2);
--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -3,7 +3,7 @@
  * imports a dictionary of occurrences by user id and computes a dictionary
  * of user ids by occurrence
  */
-const dict = require('./101-data').dict;
+const dict = require('./101-data.js').dict;
 
 const arrCounter = [];
 for (const key in dict) {
